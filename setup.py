e6850b07caef8061b299f2e76a829d9d371b9b74
Nils Le Roux <nleroux@payplug.com>
2015-09-25 18:37:00 +0200

    Fixed typo + PEP8 + version parser

--- setup.py
+++ setup.py
@@ -13,13 +13,12 @@ current_path = path.abspath(path.dirname(__file__))
 with io.open(path.join(current_path, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = 'unknown'
+__version__ = 'unknown'
 version_matcher = re.compile('^__version__\s?=\s?[\'"](.*)[\'"]$')
 with io.open(path.join(current_path, 'payplug', '__version__.py'), encoding='utf-8') as f:
     for line in f:
-        result = version_matcher.match(line)
-        if result:
-            version = result.group(1)
+        if not line.startswith('#'):
+            exec(line)
 
 
 class PyTest(test):
@@ -42,7 +41,7 @@ class PyTest(test):
 
 setup(
     name='payplug',
-    version=version,
+    version=__version__,
     description='PayPlug payment solution',
     long_description=long_description,
     url='https://github.com/payplug/payplug-python/releases',
